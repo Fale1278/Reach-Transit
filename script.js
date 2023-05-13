86e71683d116ebc220a72811d82d5a1a958b0e5f
Fale King Nanmua <kingnanmuafale@gmail.com>
2023-05-13 14:18:41 -0700

    update

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
- let btnOpen = document.querySelector(".btnOpen");
+
+/*=============== t0ggle icon topnav =============*/
+let menuIcon = document.querySelector("#menu-icon");
+let topnav = document.querySelector(".topnav");
+
+menuIcon.onclick = () =>{
+  menuIcon.classList.toggle('bx-x');
+  topnav.classList.toggle('active');
+}
+
+/* ================ scroll section active link ===================*/
+let sections = document.querySelector('section');
+let navLinks = document.querySelector("header nav a");
+
+window.onscroll = (e) =>{
+  e.preventDefault();
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop -150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute ('id');
+
+    if (top >= offset && top < offset + height){
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*= '+ id +']').classList.add('active');
+      });
+    };
+  });
+
+  /* ============== sticky topnav ==============*/
+  let header = document.querySelector('header');
+
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+
+  menuIcon.classList.remove('bx-x');
+  topnav.classList.tremove('active');
+
+};
+
+/* =============== button open for login ==============*/
+let btnOpen = document.querySelector(".btnOpen");
  let btnOpen1 = document.querySelector(".btnOpen1");
- let btnOpen2 = document.querySelector(".btnOpen2");
+ let btnOpen2 = document.querySelector(".btnOpen2"); 
+ let btnOpen3 = document.querySelector(".btnOpen3");
  let box = document.querySelector(".box");
  let box1 = document.querySelector(".box1");
  let box2 = document.querySelector(".box2");
+ let box3 = document.querySelector(".box3");
  let body = document.querySelector("body");
  let close = document.querySelector(".close");
  let close1 = document.querySelector(".close1");
  let close2 = document.querySelector(".close2");
+ let close3 = document.querySelector(".close3");
 
  btnOpen.addEventListener("click", (e)=>{
   e.preventDefault();
@@ -39,4 +84,15 @@
  close2.addEventListener("click", ()=>{
   btnOpen2.style.display="inline";
   box2.style.display="none";
+ });
+ 
+ btnOpen3.addEventListener("click", (e)=>{
+  e.preventDefault();
+  btnOpen3.style.display="none";
+  box3.style.display="block";
+ });
+
+ close3.addEventListener("click", ()=>{
+  btnOpen3.style.display="inline";
+  box3.style.display="none";
  });
