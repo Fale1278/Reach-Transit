849b47ef13989df3ed9595c0c0d2a53b81e17d32
Fale King Nanmua <kingnanmuafale@gmail.com>
2023-05-22 05:38:22 -0700

    Update

--- api.js
+++ api.js
@@ -3,9 +3,12 @@ const email = document.getElementById("email");
 const date = document.getElementById("date");
 const password = document.getElementById("password");
 const cpassword = document.getElementById("cpassword");
+const from = document.getElementById('from');
+const toCity = document.getElementById('toCity');
+const departureDate = document.getElementById("departureDate");
 
+// For handling Registration
 function handleRegister() {
-  // ... existing code
   console.log(
     username.value,
     email.value,
@@ -21,6 +24,64 @@ function handleRegister() {
       username: username.value,
       email: email.value,
       password: password.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.success) {
+        window.location.href = 'index2.html';
+      }
+    })
+    .catch((err) => console.log(err));
+}
+
+// For handling Login....POSt Method
+function handlerLogin() {
+  console.log(
+    email.value,
+    password.value,
+  );
+
+  fetch("https://reach-0hh4.onrender.com/authenticationRoute/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.success) {
+        window.location.href = 'index2.html';
+      }
+    })
+    .catch((err) => console.log(err));
+}
+
+// For handling Availability
+
+function handlerAvailability() {
+  // ... existing code
+  console.log(
+    from.value,
+    toCity.value,
+    departureDate.value,
+  );
+
+  fetch("https://reach-0hh4.onrender.com/tripRoute", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      from: from.value,
+      toCity: toCity.value,
+      departureDate: departureDate.value,
     }),
   })
     .then((res) => res.json())
