0c3017cbe821a8a0a7009aee2fd2411ab5d03d5a
Fale King Nanmua <kingnanmuafale@gmail.com>
2023-05-24 11:26:19 -0700

    Update

--- script.js
+++ script.js
@@ -55,3 +55,140 @@ file.addEventListener("change", function () {
     reader.readAsDataURL(chosenFile);
   }
 });
+
+// API consumption
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const fromCity = document.getElementById('from');
+const toCity = document.getElementById('toCity');
+const departureDate = document.getElementById("departureDate");
+
+// For handling Registration
+function handleRegister() {
+  if (!username.value || !email.value || !password.value) {
+    console.error("Please fill in all required fields.");
+    return;
+  }
+
+  fetch("https://reach-0hh4.onrender.com/authenticationRoute/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username: username.value,
+      email: email.value,
+      password: password.value
+    })
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.success) {
+        window.location.href = 'index2.html';
+      } else {
+        console.error(data.error);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+// For handling Login....POST Method
+function handlerLogin() {
+  if (!email.value || !password.value) {
+    console.error("Please fill in all required fields.");
+    return;
+  }
+
+  fetch("https://reach-0hh4.onrender.com/authenticationRoute/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value
+    })
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.success) {
+        window.location.href = 'index2.html';
+      } else {
+        console.error(data.error);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+// For handling Availability
+function handlerAvailability() {
+  if (!fromCity.value || !toCity.value || !departureDate.value) {
+    console.error("Please fill in all required fields.");
+    return;
+  }
+
+  const selectedFromCity = fromCity.value;
+  const selectedToCity = toCity.value;
+  const selectedDepartureDate = departureDate.value;
+
+  console.log(selectedFromCity, selectedToCity, selectedDepartureDate);
+
+  fetch("https://reach-0hh4.onrender.com/tripRoute", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: selectedFromCity,
+      time: selectedToCity,
+      date: selectedDepartureDate
+    })
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.success) {
+        window.location.href = 'index2.html';
+      } else {
+        console.error(data.error);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  const selectElement = document.getElementById("from");
+  const selectedOption = selectElement.options[selectElement.selectedIndex].text;
+
+  const replaceFrom = document.getElementById("replaceFrom");
+  replaceFrom.textContent = selectedOption;
+
+  const selectElement2 = document.getElementById("toCity");
+  const selectedOption2 = selectElement2.options[selectElement2.selectedIndex].text;
+
+  const replaceTo = document.getElementById("replaceTo");
+  replaceTo.textContent = selectedOption2;
+
+  const selectElement3 = document.getElementById("departureDate");
+  const selectedOption3 = selectElement3.value;
+
+  const replaceDate = document.getElementById("replaceDate");
+  replaceDate.textContent = selectedOption3;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const carSelect = document.getElementById("changeCar");
+  const carDisplay = document.getElementById("car");
+
+  carSelect.addEventListener("change", function() {
+    const selectedOption = carSelect.options[carSelect.selectedIndex].text;
+    carDisplay.textContent = selectedOption;
+  });
+});
